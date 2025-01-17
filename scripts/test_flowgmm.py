014448f8d5f845a4fb6574c75b28e54321f74b1c
Joseph Zhang <jzha@mit.edu>
2025-01-16 22:04:15 -0500

    add flowgmm test file, resolve imports

---
+++ scripts/test_flowgmm.py
@@ -0,0 +1,194 @@
+import torch
+from torch import nn
+from torch.nn.parameter import Parameter
+from torch import distributions
+from torch import inf
+
+import numpy as np
+torch.manual_seed(0)
+np.random.seed(0)
+
+import matplotlib
+import matplotlib.pyplot as plt
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 8
+rcParams['figure.dpi'] = 300
+
+# from flow_ssl.realnvp.realnvp_toy import ToyRealNVP
+from flowgmm.flow_ssl.realnvp.realnvp import RealNVPTabular
+from flowgmm.flow_ssl.data import make_circles_ssl, make_moons_ssl, make_dataset_from_img, make_dataset_from_npz
+from flowgmm.flow_ssl.distributions import SSLGaussMixture
+from flowgmm.flow_ssl import FlowLoss
+
+from itertools import chain
+
+
+
+r = 3.5
+means = torch.tensor([[-r, -r], [r, r]])
+
+# r = 3.5
+# n_classes = 2
+# means = get_means(n_classes, r)
+
+prior = SSLGaussMixture(means=means)
+
+data, labels = make_moons_ssl()
+
+flow = RealNVPTabular(num_coupling_layers=5, in_dim=2, num_layers=1, hidden_dim=512)
+loss_fn = FlowLoss(prior)
+# get_toy_nvp(D=2, prior=prior, device=None, inner_dim=256, coupling_layers_num=6, inner_layers=2)
+
+# print(data, labels)
+data = data[labels != -1]
+labels = labels[labels != -1]
+
+lr_init = 1e-4
+epochs = 2001
+
+n_ul = np.sum(labels == -1)
+n_l = np.shape(labels)[0] - n_ul
+batch_size = n_l
+print_freq = 500
+
+labeled_data = data[labels != -1]
+labeled_labels = labels[labels != -1]
+unlabeled_data = data[labels == -1]
+unlabeled_labels = labels[labels == -1]
+
+optimizer = torch.optim.Adam([p for p in flow.parameters() if p.requires_grad==True], lr=lr_init, 
+                             weight_decay=1e-2)
+for t in range(epochs):    
+    
+#     batch_idx = np.random.choice(n_ul, size=batch_size)
+#     batch_x, batch_y = unlabeled_data[batch_idx], unlabeled_labels[batch_idx]
+    batch_x = labeled_data
+    batch_y = labeled_labels
+    batch_x, batch_y = torch.from_numpy(batch_x), torch.from_numpy(batch_y)
+    z = flow(batch_x)
+    sldj = flow.logdet()
+
+    loss = loss_fn(z, sldj, batch_y)
+    
+    optimizer.zero_grad()
+    loss.backward()#retain_graph=True)
+    optimizer.step()
+    
+    if t % print_freq == 0:
+        print('iter %s:' % t, 'loss = %.3f' % loss)
+        
+    if t == int(epochs * 0.5) or t == int(epochs * 0.8):
+        for p in optimizer.param_groups:
+            p["lr"] /= 10
+
+
+
+
+def grid_image(mapping, xx, yy, extradim=False):
+    lines = np.hstack([xx.reshape([-1, 1]), yy.reshape([-1, 1])])
+    if extradim:
+        lines = lines[:, None, :]
+    lines = torch.from_numpy(lines).float()
+    img_lines = mapping(lines).detach().numpy()
+    
+    if extradim:
+        img_xx, img_yy = img_lines[:, 0, 0], img_lines[:, 0, 1]
+    else:
+        img_xx, img_yy = img_lines[:, 0], img_lines[:, 1]
+    img_xx = img_xx.reshape(xx.shape)
+    img_yy = img_yy.reshape(yy.shape)
+    return img_xx, img_yy
+
+def get_means(n_classes, r):
+    phis = np.linspace(0, 2 * np.pi, n_classes+1)[:-1]
+    mean_x = np.cos(phis) * r
+    mean_y = np.sin(phis) * r
+    means = np.hstack([mean_x[:, None], mean_y[:, None]])
+    means = torch.from_numpy(means).float()
+    return means
+from scipy.spatial.distance import cdist
+
+def get_decision_boundary(f_xx, f_yy, prior):
+    f_points = np.hstack([f_xx.reshape([-1, 1]), f_yy.reshape([-1, 1])])
+    classes = prior.classify(torch.from_numpy(f_points).float()).detach().numpy()
+    return classes
+
+plt.figure(figsize=(12, 15))
+
+grid_points = 150
+grid_freq = 10
+z_lims = np.array([-7.5, 7.5])
+x_lims = np.array([-1.5, 2.5])
+line_z = np.linspace(*z_lims, grid_points)
+line_x = np.linspace(*x_lims, grid_points)
+xx_z, yy_z = np.meshgrid(line_z, line_z)
+xx_x, yy_x = np.meshgrid(line_x, line_x)
+
+
+inv = flow(torch.from_numpy(data)).detach().numpy()
+
+plt.subplot(321)
+plt.scatter(inv[:, 0], inv[:, 1], c=inv[:, 0], cmap=plt.cm.rainbow)
+for i in range(int(np.max(labels) + 1)):
+    plt.scatter(inv[labels==i][:, 0], inv[labels==i][:, 1], marker="^", s=100, edgecolor="k")
+f_xx, f_yy = grid_image(flow, xx_x, yy_x)
+plt.plot(f_xx[:, ::grid_freq], f_yy[:, ::grid_freq], '-r', alpha=0.35)
+f_xx_, f_yy_ = grid_image(flow, yy_x, xx_x)
+plt.plot(f_xx_[:, ::grid_freq], f_yy_[:, ::grid_freq], '-b', alpha=0.35)
+plt.title(r'$z = f(X)$')
+plt.xlim(z_lims)
+plt.ylim(z_lims)
+
+plt.subplot(322)
+zs = []
+for i in range(len(means)):
+    z = loss_fn.prior.sample((1000,), gaussian_id=i).numpy()
+    zs.append(z)
+    plt.scatter(z[:, 0], z[:, 1], cmap=plt.cm.rainbow)
+plt.plot(xx_z[:, ::grid_freq], yy_z[:, ::grid_freq], '-r', alpha=0.35)
+plt.plot(yy_z[:, ::grid_freq], xx_z[:, ::grid_freq], '-b', alpha=0.35)
+for i in range(int(np.max(labels) + 1)):
+    plt.scatter(inv[labels==i][:, 0], inv[labels==i][:, 1], marker="^", s=100, edgecolor="k")
+plt.title(r'$z \sim p(z)$')
+plt.xlim(z_lims)
+plt.ylim(z_lims)
+
+plt.subplot(323)
+plt.scatter(data[:, 0], data[:, 1], c=inv[:, 0], cmap=plt.cm.rainbow)
+for i in range(int(np.max(labels) + 1)):
+    plt.scatter(data[labels==i][:, 0], data[labels==i][:, 1], marker="^", s=100, edgecolor="k")
+plt.plot(xx_x[:, ::grid_freq], yy_x[:, ::grid_freq], '-r', alpha=0.35)
+plt.plot(yy_x[:, ::grid_freq], xx_x[:, ::grid_freq], '-b', alpha=0.35)
+plt.title(r'$X \sim p(X)$')
+plt.xlim(x_lims)
+plt.ylim(x_lims)
+
+
+plt.subplot(324)
+g_xx, g_yy = grid_image(flow.inverse, xx_z, yy_z)
+plt.plot(g_xx[:, ::grid_freq], g_yy[:, ::grid_freq], '-r', alpha=0.35)
+g_xx_, g_yy_ = grid_image(flow.inverse, yy_z, xx_z)
+plt.plot(g_xx_[:, ::grid_freq], g_yy_[:, ::grid_freq], '-b', alpha=0.35)
+xs = []
+
+for z in zs:
+    x = flow.inverse(torch.from_numpy(z).float()).detach().numpy()
+    plt.scatter(x[:, 0], x[:, 1], cmap=plt.cm.rainbow)
+    xs.append(x)
+    
+for i in range(int(np.max(labels) + 1)):
+    plt.scatter(data[labels==i][:, 0], data[labels==i][:, 1], marker="^", s=100, edgecolor="k")
+    
+plt.title(r'$X = g(z)$')
+plt.xlim(x_lims)
+plt.ylim(x_lims)
+
+plt.subplot(325)
+classes = get_decision_boundary(f_xx, f_yy, prior)
+plt.contourf(xx_x, yy_x, classes.reshape(xx_x.shape), cmap="RdBu_r", alpha=0.5)
+plt.scatter(data[:, 0], data[:, 1], c=inv[:, 0], cmap=plt.cm.rainbow, alpha=0.5)
+for i in range(int(np.max(labels) + 1)):
+    plt.scatter(data[labels==i][:, 0], data[labels==i][:, 1], marker="^", s=100, edgecolor="k")
+plt.title(r'Classifier')
+plt.xlim(x_lims)
+plt.ylim(x_lims)
