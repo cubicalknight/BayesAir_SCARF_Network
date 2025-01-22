e07cd835009154231d3aeb18d00fdbe7abed50fb
Joseph Zhang <jzha@mit.edu>
2025-01-22 05:30:45 -0500

    actually just hide the plotting init

--- scripts/lga/lga_network.py
+++ scripts/lga/lga_network.py
@@ -1022,30 +1022,28 @@ def train(
 
 
     # here, we make the nominal, failure, and uniform (not used) priors...
-    fig = plt.figure()
-
+    # fig = plt.figure()
     # TODO: shifted gamma doesn't work. i have no idea why
     mst_prior_nominal = _affine_beta_dist_from_mean_std(.0125, .001, .010, .020, device)
     # mst_prior_nominal = _shifted_gamma_dist_from_mean_std(.0125, .001, .01, device)
-    s = mst_prior_nominal.sample((10000,)).detach().cpu()
-    sns.histplot(s, color='b', alpha=.33, fill=True, label="nominal", kde=True, binwidth=.0001, edgecolor='k', linewidth=0)
+    # s = mst_prior_nominal.sample((10000,)).detach().cpu()
+    # sns.histplot(s, color='b', alpha=.33, fill=True, label="nominal", kde=True, binwidth=.0001, edgecolor='k', linewidth=0)
 
     mst_prior_failure = _affine_beta_dist_from_mean_std(.0200, .002, .010, .030, device)
     # mst_prior_failure = _shifted_gamma_dist_from_mean_std(.0200, .002, .01, device)
-    s = mst_prior_failure.sample((10000,)).detach().cpu()
-    sns.histplot(s, color='r', alpha=.33, fill=True, label="failure", kde=True, binwidth=.0001, edgecolor='k', linewidth=0)
-
-    plt.savefig('ab_test.png')
+    # s = mst_prior_failure.sample((10000,)).detach().cpu()
+    # sns.histplot(s, color='r', alpha=.33, fill=True, label="failure", kde=True, binwidth=.0001, edgecolor='k', linewidth=0)
+    # plt.savefig('ab_test.png')
 
     mst_prior_default = _affine_beta_dist_from_alpha_beta(1.0, 1.0, .005, .030, device)
-    s = mst_prior_default.sample((10000,)).detach().cpu()
-    sns.histplot(s, color='purple', alpha=.33, fill=True, label="empty", kde=True, binwidth=.0001, edgecolor='k', linewidth=0)
+    # s = mst_prior_default.sample((10000,)).detach().cpu()
+    # sns.histplot(s, color='purple', alpha=.33, fill=True, label="empty", kde=True, binwidth=.0001, edgecolor='k', linewidth=0)
     
-    plt.legend()
-    plt.title("prior distributions example")
-    plt.savefig('abc_test.png')
-    plt.close(fig)
-    # return
+    # plt.legend()
+    # plt.title("prior distributions example")
+    # plt.savefig('abc_test.png')
+    # plt.close(fig)
+    # # return
 
 
     # by default, scale ELBO down by num flights
