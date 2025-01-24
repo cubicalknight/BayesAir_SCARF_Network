0bf816623aa8bf5e56efab147fb3640bf1e689d4
Joseph Zhang <jzha@mit.edu>
2025-01-24 15:32:12 -0500

    change scratchpad to give some data to micah from the earlier step

--- scripts/lga/lga_scratchpad.py
+++ scripts/lga/lga_scratchpad.py
@@ -6,6 +6,14 @@ import torch
 
 from tqdm import tqdm
 
+import bayes_air.utils.dataloader as ba_dataloader
+
+from scripts.lga.lga_network import (
+    make_travel_times_dict_and_observation_df,
+    make_states,
+)
+
+
 dir_path = Path(__file__).parent
 
 def deal_with_model_logprobs():
@@ -45,6 +53,7 @@ def deal_with_checkpoints():
     days = pd.date_range(start='2019-01-01', end='2019-12-31', freq='D').strftime('%Y-%m-%d').to_list()
 
     s_guide_dist_dict = {}
+    s_guide_dist_params_dict = {}
 
     pbar = tqdm(days)
     for name in pbar:
@@ -57,17 +66,65 @@ def deal_with_checkpoints():
             posterior_samples = s_guide()
         posterior_samples = posterior_samples["LGA_0_mean_service_time"]
 
-        mu = posterior_samples.mean().detach()
-        sigma = torch.std(posterior_samples).detach()
+        mu = posterior_samples.mean().detach().item()
+        sigma = torch.std(posterior_samples).detach().item()
         s_guide_dist = torch.distributions.Normal(mu, sigma)
-        pbar.set_description(f'{name} {mu.item():.4f} {sigma.item():.4f}')
+        pbar.set_description(f'{name} {mu:.4f} {sigma:.4f}')
 
         s_guide_dist_dict[name] = s_guide_dist
+        s_guide_dist_params_dict[name] = (mu, sigma)
 
     with open(dir_path / 'extras/2019_s_guide_dist_dict.pkl', 'wb+') as handle:
         dill.dump(s_guide_dist_dict, handle)
 
+    with open(dir_path / 'extras/2019_s_guide_dist_params_dict.pkl', 'wb+') as handle:
+        dill.dump(s_guide_dist_params_dict, handle)
+
+    df = pd.DataFrame(
+        [(k,) + v for k,v in s_guide_dist_params_dict.items()], 
+        columns=['date', 'mu', 'sigma']
+    )
+    df.to_csv(dir_path / 'extras/2019_s_guide_dist_params.csv', index=False)
+    df.to_parquet(dir_path / 'extras/2019_s_guide_dist_params.parquet')
+
+
+
+def deal_with_dataloader():
+    day_strs = pd.date_range(start='2019-1-1', end='2019-12-31', freq='D').strftime('%Y-%m-%d').to_list()
+    day_strs_list = [[day_str] for day_str in day_strs]
+    network_airport_codes = ['LGA']
+
+    output_dict = {}
+
+    pbar = tqdm(day_strs_list)
+    for day_strs in pbar:
+
+        # gather data
+        days = pd.to_datetime(day_strs)
+        data = ba_dataloader.load_remapped_data_bts(days)
+        name = ", ".join(day_strs)
+
+        num_days = len(days)
+        num_flights = sum([len(df) for df in data.values()])
+        pbar.set_description(f"{name} -> days: {num_days}, flights: {num_flights}")
+
+        # make things with the data
+        travel_times_dict, observations_df = \
+            make_travel_times_dict_and_observation_df(
+                data, network_airport_codes
+            ) 
+        states = make_states(data, network_airport_codes)
+
+        with open(dir_path / f'extras/cached_travel_times_dict/{name}.pkl', 'wb+') as handle:
+            dill.dump(travel_times_dict, handle)
+        
+        with open(dir_path / f'extras/cached_states/{name}.pkl', 'wb+') as handle:
+            dill.dump(states, handle)
+
+
+
 
 if __name__ == '__main__':
-    # deal_with_checkpoints()
-    deal_with_model_logprobs()
+    deal_with_checkpoints()
+    # deal_with_model_logprobs()
+    # deal_with_dataloader()
